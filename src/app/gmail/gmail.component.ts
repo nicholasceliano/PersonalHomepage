a82bfc2f276f9d872442e6b3e3f0fa1f6163eaa8
Nick <nicholasceliano@gmail.com>
2019-03-22 15:05:28 -0400

    Added gmail polling refresh

--- src/app/gmail/gmail.component.ts
+++ src/app/gmail/gmail.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { GoogleService } from '../_services/google.service';
 import { OAuthUrlResponse } from '../_models/oauth-url-response';
 import { GmailThread } from '../_models/gmail-thread';
-import { finalize } from 'rxjs/operators';
+import { finalize,  } from 'rxjs/operators';
+import { timer, Subscription } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-gmail',
@@ -13,12 +15,21 @@ export class GmailComponent implements OnInit {
 
   constructor(private googleService: GoogleService) { }
 
+  private pollSubscription: Subscription;
   public isPanelLoaded: boolean = false;
   public signInUrl: string;
   public googleAuthenicated: boolean = false;
   public unreadThreads: GmailThread[] = [];
-
+  
   ngOnInit() {
+    this.pollSubscription = timer(0, environment.mailPanelRefreshTime).subscribe(() => this.refreshGmailPanel());
+  }
+
+  authenticate() {
+    window.location.href = this.signInUrl;
+  }
+
+  private refreshGmailPanel() {
     var googleUserAuthUID = this.googleService.GetUserAuthUID();
     
     if (googleUserAuthUID && googleUserAuthUID.length == 36) {
@@ -27,7 +38,9 @@ export class GmailComponent implements OnInit {
           this.failedAuthentication(res.msg);
         } else {
           this.googleAuthenicated = true;
-          this.unreadThreads = res.data;
+          
+          if (JSON.stringify(this.unreadThreads) !== JSON.stringify(res.data))
+            this.unreadThreads = res.data;
         }
       }, (err) => {
         this.failedAuthentication(err);
@@ -37,12 +50,9 @@ export class GmailComponent implements OnInit {
     }
   }
 
-  authenticate() {
-    window.location.href = this.signInUrl;
-  }
-
   private failedAuthentication(err?: string) {
     this.googleAuthenicated = false;
+    this.unreadThreads = [];
     this.googleService.GetOAuth2SignInUrl().pipe(finalize(() => this.isPanelLoaded = true)).subscribe((res: OAuthUrlResponse) => {
       this.signInUrl = res.url;
     });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
   oauthCookiesName: {
     google: "googleAuthUID"
   },
+  mailPanelRefreshTime:30000,
   twitchAuthUri:"https://id.twitch.tv/oauth2",//TODO: This is temporary - Need to move all the twitch oAuth to server
   twitchAPIv5Uri:"https://api.twitch.tv/kraken",
   twitchClientId:"x15tbpitfdkjhoiotp5mdivv7ukq5n",
