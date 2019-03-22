24ef07af5878a6b3a52a3eaf47a48c87eb205ef4
Nick <nicholasceliano@gmail.com>
2019-03-22 12:30:19 -0400

    Added gmail panel ui

--- src/app/gmail/gmail.component.ts
+++ src/app/gmail/gmail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GoogleService } from '../_services/google.service';
 import { OAuthUrlResponse } from '../_models/oauth-url-response';
 import { GmailThread } from '../_models/gmail-thread';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-gmail',
@@ -12,27 +13,42 @@ export class GmailComponent implements OnInit {
 
   constructor(private googleService: GoogleService) { }
 
+  public isPanelLoaded: boolean = false;
   public signInUrl: string;
-  public loginRequired: boolean = true;
+  public googleAuthenicated: boolean = false;
   public unreadThreads: GmailThread[] = [];
 
   ngOnInit() {
     var googleUserAuthUID = this.googleService.GetUserAuthUID();
     
     if (googleUserAuthUID && googleUserAuthUID.length == 36) {
-      this.loginRequired = false;
-      this.googleService.GetUnreadEmails().subscribe((res) => {
-        this.unreadThreads = res.data;
+      this.googleService.GetUnreadEmails().pipe(finalize(() => this.isPanelLoaded = true)).subscribe((res) => {
+        if (res.err) {
+          this.failedAuthentication(res.msg);
+        } else {
+          this.googleAuthenicated = true;
+          this.unreadThreads = res.data;
+        }
+      }, (err) => {
+        this.failedAuthentication(err);
       });
     } else {
-      this.loginRequired = true;
-      this.googleService.GetOAuth2SignInUrl().subscribe((res: OAuthUrlResponse) => {
-        this.signInUrl = res.url;
-      });
+      this.failedAuthentication();
     }
   }
 
-  signIn() {
+  authenticate() {
     window.location.href = this.signInUrl;
+  }
+
+  private failedAuthentication(err?: string) {
+    this.googleAuthenicated = false;
+    this.googleService.GetOAuth2SignInUrl().pipe(finalize(() => this.isPanelLoaded = true)).subscribe((res: OAuthUrlResponse) => {
+      this.signInUrl = res.url;
+    });
+
+    if (err) {
+      console.log(err);
+    }
   }
 }
