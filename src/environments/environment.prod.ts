367092ef25d7bcad906e7327a8d71bd8a1ccd5e8
Nick <nicholasceliano@gmail.com>
2019-10-13 15:07:08 -0400

    added prod deploy script

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -17,4 +17,3 @@ export const environment = {
 	twitchPlayerAPIEndpoint: 'https://player.twitch.tv/js/embed/v1.js',
 	twitchChatWSEndpoint: 'wss://irc-ws.chat.twitch.tv:443'
 };
-// sudo ng build --prod --outputPath /var/www/html/
